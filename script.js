0753e0a32d2b4608c41e9df60c701dba9dfc794b
ForeytKL22 <krforeyt@gmail.com>
2021-09-03 21:09:01 -0600

    got local storage to save

--- script.js
+++ script.js
@@ -1,41 +1,58 @@
-
 var currentDate = moment().format('LLL'); 
 document.getElementById("currentDay").innerHTML = currentDate;
 
 
 
 
-// $(".description").on("click", function() {
-//     var text = $(this).text().trim();
-//     var id = $(this).attr("id");
-    
-
-// }); 
-
-
 
 
 $(".saveBtn").on("click", function() {
-    var hour = $(this).prev().attr("id");
+    var hourBlock = $(this).prev().attr("id");
     var description = $(this).prev().val();
-
-    localStorage.setItem(hour, description);
-
-    var retrieveDescription = localStorage.getItem(hour);
-    console.log('retrieved: ' + retrieveDescription);
+    localStorage.setItem(hourBlock, description);
 
 });
 
 
+var loadDescription = function() {
+    var retrieveDescription8 = localStorage.getItem("8-hour");
+    document.getElementById("8-hour").innerHTML = retrieveDescription8;
+    var retrieveDescription9 = localStorage.getItem("9-hour");
+    document.getElementById("9-hour").innerHTML = retrieveDescription9;
+    var retrieveDescription10 = localStorage.getItem("10-hour");
+    document.getElementById("10-hour").innerHTML = retrieveDescription10;
+    var retrieveDescription11 = localStorage.getItem("11-hour");
+    document.getElementById("11-hour").innerHTML = retrieveDescription11;
+    var retrieveDescription12 = localStorage.getItem("12-hour");
+    document.getElementById("12-hour").innerHTML = retrieveDescription12;
+    var retrieveDescription1 = localStorage.getItem("1-hour");
+    document.getElementById("1-hour").innerHTML = retrieveDescription1;
+    var retrieveDescription2 = localStorage.getItem("2-hour");
+    document.getElementById("2-hour").innerHTML = retrieveDescription2;
+    var retrieveDescription3 = localStorage.getItem("3-hour");
+    document.getElementById("3-hour").innerHTML = retrieveDescription3;
+    var retrieveDescription4 = localStorage.getItem("4-hour");
+    document.getElementById("4-hour").innerHTML = retrieveDescription4;
+    var retrieveDescription5 = localStorage.getItem("5-hour");
+    document.getElementById("5-hour").innerHTML = retrieveDescription5;
+};
 
-// var saveSchedule = function() {
-//     localStorage.setItem("schedule", description); 
-// };
-// document.querySelector(".saveBtn").addEventListener("click", saveSchedule());
-// console.log(localStorage);
+loadDescription();
 
 
-// var saveSchedule = document.querySelector(".description").value;
-// localStorage.setItem("schedule", saveSchedule);
-// console.log(localStorage);
+
+// var colorTimes = function(time) {
+//     var timeSlot = $(time).find("")
+// }
+
+var checkHour = querySelector(".description");
+var currentHour = parseInt(moment().format('LT'));
+
+
+
+// $(".description").each(function(){
+//     if (currentHour < )
+// });
+
+
 
