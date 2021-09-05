096b43404094ad49b34523d1baefd878870a112b
ForeytKL22 <krforeyt@gmail.com>
2021-09-05 12:10:20 -0600

    completed color codes

--- script.js
+++ script.js
@@ -5,54 +5,58 @@ document.getElementById("currentDay").innerHTML = currentDate;
 
 
 
-
 $(".saveBtn").on("click", function() {
     var hourBlock = $(this).prev().attr("id");
     var description = $(this).prev().val();
     localStorage.setItem(hourBlock, description);
-
 });
 
 
 var loadDescription = function() {
-    var retrieveDescription8 = localStorage.getItem("8-hour");
-    document.getElementById("8-hour").innerHTML = retrieveDescription8;
-    var retrieveDescription9 = localStorage.getItem("9-hour");
-    document.getElementById("9-hour").innerHTML = retrieveDescription9;
-    var retrieveDescription10 = localStorage.getItem("10-hour");
-    document.getElementById("10-hour").innerHTML = retrieveDescription10;
-    var retrieveDescription11 = localStorage.getItem("11-hour");
-    document.getElementById("11-hour").innerHTML = retrieveDescription11;
-    var retrieveDescription12 = localStorage.getItem("12-hour");
-    document.getElementById("12-hour").innerHTML = retrieveDescription12;
-    var retrieveDescription1 = localStorage.getItem("1-hour");
-    document.getElementById("1-hour").innerHTML = retrieveDescription1;
-    var retrieveDescription2 = localStorage.getItem("2-hour");
-    document.getElementById("2-hour").innerHTML = retrieveDescription2;
-    var retrieveDescription3 = localStorage.getItem("3-hour");
-    document.getElementById("3-hour").innerHTML = retrieveDescription3;
-    var retrieveDescription4 = localStorage.getItem("4-hour");
-    document.getElementById("4-hour").innerHTML = retrieveDescription4;
-    var retrieveDescription5 = localStorage.getItem("5-hour");
-    document.getElementById("5-hour").innerHTML = retrieveDescription5;
+    var retrieveDescription8 = localStorage.getItem("8");
+    document.getElementById("8").innerHTML = retrieveDescription8;
+    var retrieveDescription9 = localStorage.getItem("9");
+    document.getElementById("9").innerHTML = retrieveDescription9;
+    var retrieveDescription10 = localStorage.getItem("10");
+    document.getElementById("10").innerHTML = retrieveDescription10;
+    var retrieveDescription11 = localStorage.getItem("11");
+    document.getElementById("11").innerHTML = retrieveDescription11;
+    var retrieveDescription12 = localStorage.getItem("12");
+    document.getElementById("12").innerHTML = retrieveDescription12;
+    var retrieveDescription1 = localStorage.getItem("13");
+    document.getElementById("13").innerHTML = retrieveDescription1;
+    var retrieveDescription2 = localStorage.getItem("14");
+    document.getElementById("14").innerHTML = retrieveDescription2;
+    var retrieveDescription3 = localStorage.getItem("15");
+    document.getElementById("15").innerHTML = retrieveDescription3;
+    var retrieveDescription4 = localStorage.getItem("16");
+    document.getElementById("16").innerHTML = retrieveDescription4;
+    var retrieveDescription5 = localStorage.getItem("17");
+    document.getElementById("17").innerHTML = retrieveDescription5;
+}; 
+
+
+
+
+
+function colorTimes() {
+    for (let i = 8; i <= 17; i++) {
+        var checkHour = $('#' + i);
+        var currentHour = parseInt(moment().format('LT'));
+
+        if (i < currentHour) {
+            checkHour.addClass("past");
+        } else if (i === currentHour) {
+            checkHour.addClass("present");
+        } else {
+            checkHour.addClass("future");
+        }
+    }
 };
 
 loadDescription();
+colorTimes();
 
-
-
-// var colorTimes = function(time) {
-//     var timeSlot = $(time).find("")
-// }
-
-var checkHour = querySelector(".description");
-var currentHour = parseInt(moment().format('LT'));
-
-
-
-// $(".description").each(function(){
-//     if (currentHour < )
-// });
 
 
 
